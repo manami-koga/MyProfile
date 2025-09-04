0423406721c69fa810ad1082b9ce2f25b4d78bca
Manami.koga <arigatou0712@outlook.jp>
2025-09-04 22:28:32 +0900

    HomeButtonの修正

--- src/pages/components/HomeButton.tsx
+++ src/pages/components/HomeButton.tsx
@@ -1,9 +1,10 @@
 import buttonStyles from "../../styles/buttonStyles.module.css"
+import Link from 'next/link';
 
 const HomeButton = () => {
     return (
         <div className={buttonStyles.button}>
-            <a href="/Home" className="btn">ホームに戻る</a>
+            <Link href="/Home/">Home</Link>
         </div>
     )
 }
